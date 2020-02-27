333b7b3869952370f16f62876a18fac25fb6e09b
D J <dominik.jonke@outlook.com>
2020-02-27 13:13:53 +0100

    New List Student

---
+++ LinqSimpleExercise/LinqSimpleExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqSimpleExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> student = new List<Student>();
+
+            student.Add(new Student
+            {
+                ID = "1",
+                Name = "Dominik"
+            });
+
+            student.Add(new Student
+            {
+                ID = "2",
+                Name = "Janine"
+            });
+
+            student.Add(new Student
+            {
+                ID = "3",
+                Name = "Rene"
+            });
+
+            var result = from s in student
+                            where s.ID.Contains("3")
+                            select s.Name;
+
+            Console.WriteLine(result.First());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinqSimpleExercise/LinqSimpleExercise/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqSimpleExercise
+{
+    internal class Student
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
